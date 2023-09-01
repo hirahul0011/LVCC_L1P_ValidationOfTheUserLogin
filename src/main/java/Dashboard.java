a31bf06fa345fbe209e64798ac513088f9562cf8
hirahul0011 <ra_10001@yahoo.co.in>
2023-09-01 14:23:51 +0530

    Dashboard servlet is created for the successful login

---
+++ src/main/java/Dashboard.java
@@ -0,0 +1,50 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Dashboard
+ */
+public class Dashboard extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Dashboard() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session=request.getSession();
+		String username=(String)session.getAttribute("username");
+		PrintWriter out=response.getWriter();
+		out.println("<html><body>");
+	    out.println("Welcome "+username+"!!<br>");
+	    out.println("</body></html>");
+//		out.println("<center><span style='color:red'>Welcome "+username+"!!</span></center>"); //code is used to append
+	    																//something on the existing page
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
