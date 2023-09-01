88c2764d188d7f4be6f750fe56342f24f71393ee
hirahul0011 <ra_10001@yahoo.co.in>
2023-09-01 13:59:13 +0530

    Login Controller servlet created to handle invalid logins and to show appropriate error messages

---
+++ src/main/java/LoginController.java
@@ -0,0 +1,54 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginController
+ */
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session=request.getSession();  
+		String username=request.getParameter("username");
+		String password=request.getParameter("password");
+		
+		if(username.equalsIgnoreCase("donald") && password.equals("duck")) {
+			
+		}else {			
+			PrintWriter out=response.getWriter();
+			request.getRequestDispatcher("index.html").include(request, response);
+			out.println("<center><span style='color:red'>Invalid Credentials!!</span></center>");
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
